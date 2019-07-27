d859202f4d94fc68d705290778fe952559fc35d8
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2019-07-28 01:39:36 +0530

    Added howler to play songs

--- recent.js
+++ recent.js
@@ -2,13 +2,14 @@ fs.readdir(dest, function(err, files){
     for(let i = 0; i < files.length; i++){
         file = files[i];
         if(path.extname(file) == ".mp3"){
+            recent.songs_src.push(`${dest}/${file}`);
             jsmediatags.read(`${dest}/${file}`, {
                 onSuccess: function(tag){
+                    let ret = recent.reach;
                     tag.tags.picture.data = getAlbumArt(tag);
-                    if(recent.songs.length < 5 && tag.tags.artist == "Jonas Brothers"){
-                        recent.songs.push(tag.tags);
-                        console.log(tag.tags);
-                    }
+                    tag.tags.src = recent.songs_src[ret];
+                    recent.songs.push(tag.tags);
+                    recent.reach++;
                 }
             });
         }
@@ -18,6 +19,22 @@ fs.readdir(dest, function(err, files){
 let recent = new Vue({
     el: "#recent",
     data: {
-        songs: []
+        reach: 0,
+        songs: [],
+        songs_src: []
+    },
+    methods: {
+        playMe: function(src){
+            let song = new Howl({
+                src: [src]
+            });
+
+            song.play();
+        }
     }
 })
+
+// let song = new Howl({
+//     src: [src]
+// })
+// song.play();
