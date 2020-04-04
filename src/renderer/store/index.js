35ccced2efcb02ba89e4a2699bfdb69aea963081
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-04-04 14:35:36 +0530

    Added required packages and organized a bit

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -3,12 +3,20 @@ import Vuex from 'vuex'
 
 import { createPersistedState, createSharedMutations } from 'vuex-electron'
 
+import state from './state'
+import actions from './actions'
 import modules from './modules'
+import getters from './getters'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules,
+  state,
+  actions,
+  getters,
+  mutations,
   plugins: [
     createPersistedState(),
     createSharedMutations()
