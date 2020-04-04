4521ffd8b9f7a8232a43a74897b9a6efa09290aa
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-04-04 13:15:07 +0530

    Resetted project

---
+++ src/renderer/app.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <TitleBar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TitleBar from '@/components/titlebar'
+
+export default {
+  name: 'mjam',
+  components: {
+    TitleBar
+  }
+}
+</script>
+
+<style>
+  /* CSS */
+  
+body {
+  display: grid;
+  height: 100vh;
+  grid-template-rows: 30px auto 80px;
+}
+
+
+button {
+  outline: none;
+}
+
+/* latin */
+@font-face {
+  font-family: 'Salsa';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Salsa-Regular'), url(./assets/fonts/Salsa.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: url(~@/assets/fonts/Material-Icons.woff2) format('woff2');
+}
+
+::-webkit-scrollbar {
+    display: none;
+}
+
+/* latin-ext */
+@font-face {
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(~@/assets/fonts/Source-Sans-Pro.woff2) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(~@/assets/fonts/Source-Sans-Pro-Latin.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  vertical-align: middle;
+  -moz-font-feature-settings: 'liga';
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
---
+++ src/renderer/components/titlebar.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="title-bar-wrapper">
+    <div class="title-bar">
+      <h6 class="brand-logo">
+        <img src="~@/assets/mjam-logo.png" width="19px">
+        <span>MJam</span>
+      </h6>
+      <h6 class="song-title">{{ title }}</h6>
+
+      <div class="action-buttons">
+        <span class="minimize"><i class="material-icons">remove</i></span>
+        <span class="restore"><i class="material-icons">crop_landscape</i></span>
+        <span class="close"><i class="material-icons">close</i></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Slow Hands - Niall Horan'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.title-bar {
+  background: #fafafa;
+  display: flex;
+  font-family: Salsa, sans-serif;
+  position: fixed;
+  width: 100vw;
+}
+
+.title-bar .brand-logo {
+  cursor: pointer;
+  width: fit-content;
+  margin: 5px;
+}
+
+.title-bar .brand-logo > * {
+  vertical-align: middle;
+}
+
+.title-bar .song-title {
+  font-family: 'Source Sans Pro', sans-serif;
+  flex: 1;
+  -webkit-app-region: drag;
+  line-height: 29px;
+  margin: 0;
+  vertical-align: middle;
+  text-align: center;
+}
+
+.title-bar .action-buttons {
+  margin: 5px;
+  display: inline-flex;
+  justify-content: flex-end;
+}
+
+.title-bar .action-buttons span {
+  vertical-align: middle;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 10px;
+  border-radius: 50%;
+  color: white;
+  height: 10px;
+  font-size: 10px;
+  cursor: pointer;
+}
+
+.title-bar .action-buttons .minimize {
+  background: #2acb43;
+  border: 2px solid #2f9e3e;
+  margin-right: 6px;
+}
+
+.title-bar .action-buttons .restore {
+  background: #febf30;
+  border: 2px solid #d6a642;
+  margin-right: 6px;
+}
+
+.title-bar .action-buttons .close {
+  background: #ff6258;
+  border: 2px solid #be605b;
+}
+
+.title-bar .action-buttons .material-icons {
+  font-size: 10px;
+}
+</style>
