2234307426d372a985680f41360210fa4c37dc9d
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-04-10 13:48:40 +0530

    Github Pages Setup

--- scripts/constants.js
+++
@@ -1,6 +0,0 @@
-const path = require("path")
-
-module.exports = {
-  CACHED_FILE_SRC: path.resolve(__dirname, "../.user/cache.data.json"),
-  RECENT_SONGS_FILE_SRC: path.resolve(__dirname, "../.user/recent.data.json")
-}
