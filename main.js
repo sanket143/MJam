39e36688c4520777439446e167e5a278d8b6f498
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-04-11 11:50:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -48,6 +48,3 @@ app.on('activate', function () {
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
