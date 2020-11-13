da379222057e6aeeb2131167d1c892dff23539c9
Ajaysinh Rajput <54072689+AjaysinhRajput2802@users.noreply.github.com>
2020-11-13 17:18:55 +0530

    Update content.js

--- scripts/components/content.js
+++ scripts/components/content.js
@@ -44,7 +44,7 @@ const content_frame = new Vue({
     },
     all_songs(){
       console.log("Updated")
-      return Object.values(state.songsMap).sort() //Now we have sorted list of all_songs
+      return Object.values(state.songsMap)
     },
     recent_songs(){
       return state.recentSongs
