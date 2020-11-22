b83a843ce31fe0148a80da7cfb60a10c25d22543
Ajaysinh Rajput <54072689+AjaysinhRajput2802@users.noreply.github.com>
2020-11-22 23:01:22 +0530

    feat: server to serve music files
    * issue fixed
    
    * Fixed Issue Volume tweaker is not working #32
    
    * removed unnecessary things
    
    * fixed issue #41
    
    * Sorted Songs in Songs list
    
    * added things
    
    * fixed bug
    
    * made sorted
    
    * fixed bug
    
    * Update nowplaying.js
    
    * Update setup.js
    
    * Update state.js
    
    * Update nowplaying.css
    
    * Update index.html
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update app.js
    
    * Update app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const { readJSON } = require("./scripts/methods")
+const constants = require("./scripts/constants")
+const express = require('express')
+const app = express()
+
+app.get('/song', async (req, res) => {
+  const { path } = req.query
+
+  await readJSON(constants.CACHED_FILE_SRC)
+    .then((jsonData) => {
+      if(jsonData[path]){
+        res.sendFile(path)
+      } else {
+        res.send("We don't serve non-mp3 files")
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+let server = app.listen(3000, function () {
+  console.log('Express server listening on port ' + server.address().port)
+})
